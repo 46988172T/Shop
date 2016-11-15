361b469006c51fd821cf15bb3dba81ebc82db753
Leonardo Martínez Ubal <leonardomartinezubal3@gmail.com>
2016-11-15 14:27:45 +0100

    Pequeño cambio en el MainActivity para que no se guarde el error.

--- app/src/main/java/com/momentumvinum/shop/MainActivity.java
+++ app/src/main/java/com/momentumvinum/shop/MainActivity.java
@@ -2,13 +2,12 @@ package com.momentumvinum.shop;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
 
-import com.momentumvinum.shop.retrofit.RetrofitMomentum;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        RetrofitMomentum retrofitMomentum = new RetrofitMomentum();
-        retrofitMomentum.downloadCategories(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
